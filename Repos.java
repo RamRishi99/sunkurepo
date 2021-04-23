9f85e93052d627bc1ed15e4d36341948d99f3f49
ramrishi <s.ramrishi@gmail.com>
2021-04-23 15:31:33 +0530

    first

---
+++ Repos.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Repos
+{
+public static void add(int a,int b)
+{
+System.out.println(a+b);
+
+}
+public static void sub(int a,int b)
+{
+System.out.println(a-b);
+}
+public static void main(String[]args)
+{
+add(10,5);
+sub(10,5);
+}
+}
